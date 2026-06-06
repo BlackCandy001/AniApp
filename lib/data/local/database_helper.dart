d49f72f5de4e16966f229fae68a23ef690e2f77c
Ninja sound 🎶 <194749774+BlackCandy001@users.noreply.github.com>
2026-06-06 13:41:58 +0700

    rename database file to aniapp.db

--- lib/data/local/database_helper.dart
+++ lib/data/local/database_helper.dart
@@ -18,7 +18,7 @@ class DatabaseHelper {
 
   Future<Database> get database async {
     if (_database != null) return _database!;
-    _database = await _initDB('animetracker.db');
+    _database = await _initDB('aniapp.db');
     return _database!;
   }
 
